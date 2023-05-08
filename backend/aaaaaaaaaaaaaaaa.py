4ce7c1a063068258ee414ba002b196eacca6dd22
mesor21 <menzdd21@gmail.com>
2023-05-08 03:49:32 +0300

    add image

--- backend/aaaaaaaaaaaaaaaa.py
+++ backend/aaaaaaaaaaaaaaaa.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify, render_template
+import os
 
 app = Flask(__name__)
+IMG_FOLDER = os.path.join('static', 'IMG')
 
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'img.png')
     if request.method == 'POST':
         num1 = request.form['num1']
         num2 = request.form['num2']
@@ -16,9 +20,9 @@ def index():
             result = int(num1) * int(num2)
         if methodd == 'division':
             result = int(num1) / int(num2)
-        return render_template('calculatorr.html', result=result)
+        return render_template('calculatorr.html', result=result, image=full_filename)
     else:
-        return render_template('calculatorr.html')
+        return render_template('calculatorr.html',image=full_filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
